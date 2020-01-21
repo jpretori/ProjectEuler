03e385f535f0770354c8ee64c0f791c861cff560
Johan Pretorius <pretoriusjf@gmail.com>
2020-01-21 16:20:42 +0200

    Added config file to reduce reliance on cli arguments

--- src/jpretori/projecteuler/Problem002.java
+++ src/jpretori/projecteuler/Problem002.java
@@ -1,7 +1,7 @@
 package jpretori.projecteuler;
 
 public class Problem002 {
-    public long sumEvenFibo(long max) {
+    public long sumEvenFiboUpToMaxN(long max) {
     	long fibo_small = 1;
     	long fibo_large = 2;
     	long fibo_next = 3;
@@ -9,6 +9,27 @@ public class Problem002 {
     	long even_fibo_sum = 0;
 	
     	while (fibo_large <= max) {
+    		if ((fibo_large % 2) == 0) {
+    			even_fibo_sum += fibo_large;
+    		}
+    		
+    		fibo_small = fibo_large;
+    		fibo_large = fibo_next;
+    		fibo_next = fibo_small + fibo_large;
+    	}
+    	return even_fibo_sum;			
+    }
+    
+    public long sumEvenFiboUpToNthFibo(int max_fibo_index) {
+    	long fibo_small = 1;
+    	long fibo_large = 2;
+    	long fibo_next = 3;
+
+    	long even_fibo_sum = 0;
+	
+    	int fibo_large_index = 2;
+    	
+    	while (fibo_large_index++ <= max_fibo_index) {
     		if ((fibo_large % 2) == 0) {
     			even_fibo_sum += fibo_large;
     		}
--- src/jpretori/projecteuler/ProjectEuler.java
+++ src/jpretori/projecteuler/ProjectEuler.java
@@ -1,32 +1,60 @@
 package jpretori.projecteuler;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ProjectEuler {
 
-	public static void main(String[] args) {
-		problem001(args);
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Properties p = new Properties();
+		InputStream is = ProjectEuler.class.getResourceAsStream("/projecteuler.cfg");
+		p.load(is);
+				
+		problem001(p);
 		System.out.println("------- ------- -------");
-    	problem002(args);
+		problem002_ProjectEuler(p);
 		System.out.println("------- ------- -------");
-    	problem003(args);
+		problem003(p);
 		System.out.println("------- ------- -------");
 	}
 
-	private static void problem001(String[] args) {
+	private static void problem001(Properties p) {
 		Problem001 pt = new Problem001();
-		System.out.println("Problem 001; multiples of 3 and 5 up to [" + args[0] + "] = ["
-				+ pt.multiplesOf3and5(Long.parseLong(args[0])) + "]");
+		long problem001 = Long.parseLong(p.getProperty("problem001"));
+		System.out.println("Problem 001; multiples of 3 and 5 up to [" + problem001 + "] = ["
+				+ pt.multiplesOf3and5(problem001) + "]");
 	}
 
-	private static void problem002(String[] args) {
+	private static void problem002_ProjectEuler(Properties p) {
 		Problem002 pt = new Problem002();
-		System.out.println("Problem 002; sum even Fibonacci numbers up to [" + args[1] + "] = ["
-				+ pt.sumEvenFibo(Long.parseLong(args[1])) + "]");   	
+		long problem002 = Long.parseLong(p.getProperty("problem002"));
+		System.out.println("Problem 002 (projecteuler.net version); " + "sum even Fibonacci numbers up to [" + problem002
+				+ "] = [" + pt.sumEvenFiboUpToMaxN(problem002) + "]");
+
+		int problem002_fcc1 = Integer.parseInt(p.getProperty("problem002-fcc1"));
+		System.out.println("Problem 002 (freecodecamp version); " + "sum even Fibonacci numbers up to [" + problem002_fcc1
+				+ "]th Fibonacci number = [" + pt.sumEvenFiboUpToNthFibo(problem002_fcc1) + "]");
+		
+		int problem002_fcc2 = Integer.parseInt(p.getProperty("problem002-fcc2"));
+		System.out.println("Problem 002 (freecodecamp version); " + "sum even Fibonacci numbers up to [" + problem002_fcc2
+				+ "]th Fibonacci number = [" + pt.sumEvenFiboUpToNthFibo(problem002_fcc2) + "]");
+		
+		int problem002_fcc3 = Integer.parseInt(p.getProperty("problem002-fcc3"));
+		System.out.println("Problem 002 (freecodecamp version); " + "sum even Fibonacci numbers up to [" + problem002_fcc3
+				+ "]th Fibonacci number = [" + pt.sumEvenFiboUpToNthFibo(problem002_fcc3) + "]");
+		
+		int problem002_fcc4 = Integer.parseInt(p.getProperty("problem002-fcc4"));
+		System.out.println("Problem 002 (freecodecamp version); " + "sum even Fibonacci numbers up to [" + problem002_fcc4
+				+ "]th Fibonacci number = [" + pt.sumEvenFiboUpToNthFibo(problem002_fcc4) + "]");
 	}
-	
-	private static void problem003(String[] args) {
+
+	private static void problem003(Properties p) {
 		Problem003 pt = new Problem003();
-		System.out.println("Problem 003; largest prime factor of [" + args[2] + "] = ["
-				+ pt.largestPrimeFactor(Long.parseLong(args[2])) + "]");
+		long problem003 = Long.parseLong(p.getProperty("problem003"));
+		System.out.println("Problem 003; largest prime factor of [" + problem003 + "] = ["
+				+ pt.largestPrimeFactor(problem003) + "]");
 	}
 
 }
